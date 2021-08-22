d9f2b9aa8cc6fd536782d21a7b19ea912b2732eb
alpysI <alperensyesil@gmail.com>
2021-08-22 12:44:46 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kaç_basamaklı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc = 0;
+            int toplam = 0;
+            Console.Write("Sayı Girin : ");
+            int sayi = int.Parse(Console.ReadLine());
+            while (sayi > 0)
+            {
+                toplam = (sayi % 10);
+                sonuc = toplam + sayi;
+                
+                //sayi = sayi / 10;
+            }
+            Console.WriteLine("Girdiğiniz Sayının Rakamları Toplamı : " + sonuc);
+            Console.ReadKey();
+
+
+        }
+    }
+}
